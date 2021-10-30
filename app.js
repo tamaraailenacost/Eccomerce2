9d0f257219687344ea6e6730277e53ac970b2852
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2021-10-30 21:21:50 +0200

    istalo Handlebars con template AdminPro

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const { Server } = require('./src/Server')
 
 
 const server = new Server()
+
+//Handlebars
+const handlebars = require('express-handlebars');
+
+server.app.engine('handlebars', handlebars());
+server.app.set('view engine', 'handlebars');
+
+server.app.get('/', function(req, res) {
+    res.render('index');
+});
